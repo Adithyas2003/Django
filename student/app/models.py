49ba7c8be18dccb82261cf226505d5d51afb3790
Adithyas2003 <adathyas49@gmail.com>
2024-11-05 11:34:31 +0530

    dataprogram

--- student/app/models.py
+++ student/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    roll_no=models.IntegerField()
+    #name=models.CharField(max_length=20)
+    name=models.TextField()
+    age=models.IntegerField()
+    address=models.TextField()
+    class_name=models.IntegerField()
--- student/app/views.py
+++ student/app/views.py
@@ -14,3 +14,30 @@ def home(req):
         return redirect(home)
     else:
         return render(req,'home.html',{'student':std})
+    
+
+def edit_std(req,a):
+    data=''
+    for i in std:
+        if i['roll_no']==a:
+            data=i
+    if req.method=='POST':
+        roll=req.POST['roll_no']
+        name=req.POST['name']
+        age=req.POST['age']
+        address=req.POST['address']
+        class_name=req.POST['class_name']
+        data['roll_no']=roll
+        data['name']=name
+        data['age']=age
+        data['address']=address
+        data['class_name']=class_name
+        return redirect(home)
+    else:
+        return render(req,'edit.html',{'data':data})
+    
+def delete(req,a):
+    for i in std:
+        if i['roll_no']==a:
+            std.remove(i)
+    return redirect(home)
