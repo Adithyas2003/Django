ebaeb84d47a918ba585ddba24d453f7368444198
Adithyas2003 <adathyas49@gmail.com>
2024-11-27 11:34:11 +0530

    fileadded

---
+++ builtforms/app/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import *
+
+class normal_form(forms.Form):
+    Name=forms.CharField()
+    user_age=forms.IntegerField()
+    email=forms.EmailField()
+    place=forms.CharField()
+
+
+class model_form(forms.ModelForm):
+    class Meta:
+        model=project_user
+        fields='__all__'
+        # fields=['name','age']
